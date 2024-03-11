a90e29df98e985bc3ff7ad7befcc73c70a16415a
JacquesOP <jacquesoprince.dev@gmail.com>
2024-03-11 01:43:51 +0100

    update frontend

--- screens/Profile/Identity.js
+++ screens/Profile/Identity.js
@@ -13,11 +13,12 @@ import {
   TouchableOpacity,
   Modal,
 } from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 /*
 ============ Import Redux ============ 
 */
-
+import { useSelector, useDispatch } from "react-redux"
+import user, { updateIdentity, updateUser } from "../../reducers/user"
 /*
 ============ Import Modules ============ 
 */
@@ -41,6 +42,7 @@ export default function Identity({ navigation }) {
   const [withFilter, setWithFilter] = useState(true)
   const [withAlphaFilter, setWithAlphaFilter] = useState(false)
   const [withCallingCode, setWithCallingCode] = useState(false)
+  
   const onSelect = (country) => {
     setCountryCode(country.cca2)
     setCountry(country)
@@ -71,14 +73,107 @@ export default function Identity({ navigation }) {
     setStartedDate(propDate);
   }
 
+  const userFields = [
+    "phoneNumber",
+    "name",
+    "firstName",
+    "birthDate",
+    "nationality",
+    "countryBirth",
+    "familySituation",
+    "birthTown",
+    "birthDistrict",
+    "socialSecurityNumber",
+  ]
 
+  let initialState = {}
+  let initialFocusState = {}
+  userFields.forEach((field) => {
+    initialState[field] = ""
+    initialFocusState[field] = false
+  })
+  const [userData, setUserData] = useState(initialState)
+  const [inputFocused, setInputFocused] = useState(initialFocusState)
+  const [infosUser, setInfosUser] = useState("")
+  const dispatch = useDispatch()
+  const token = useSelector((state) => state.user.value.token)
+
+  useEffect(() => {
+    if (token) {
+      const url = `http://192.168.1.178:3000/users/${token}`
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          let fetchedData = {}
+          userFields.forEach((field) => {
+            fetchedData[field] =
+              data.identity && data.identity[field] ? data.identity[field] : ""
+          })
+          setInfosUser(data) // If needed
+          setUserData(fetchedData)
+        })
+    }
+  }, [token])
+
+  const handleUpdate = async () => {
+    try {
+      const identityData = {}
+      userFields.forEach((field) => {
+        identityData[field] =
+          field === "phoneNumber" ? Number(userData[field]) : userData[field]
+      })
+
+      const dataForBackend = { identity: identityData } // Now sending only identity object to your backend
+
+      const response = await fetch(
+        `http://192.168.1.25:3000/users/update/${token}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(dataForBackend),
+        }
+      )
+
+      if (!response.ok) {
+        const errorMessage = await response.json()
+        throw new Error("Error in response")
+      }
+
+      const updatedItem = await response.json()
+      dispatch(updateUser(updatedItem))
+      Alert.alert(
+        "Update Successful",
+        "Your information has been successfully updated.",
+        [{ text: "OK" }]
+      )
+    } catch (error) {
+      console.error("Error", error.message)
+    }
+  }
+
+  const handleChange = (fieldName, value) => {
+    setUserData((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }))
+  }
+  const handleFocus = (fieldName) => {
+    setInputFocused({ ...inputFocused, [fieldName]: true })
+  }
 
   return (
-    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <ScrollView style={{ width: '100%'}}>
-          <View>
-            <Text>Les informations du compte</Text>
-          </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 20}
+      >
+        <ScrollView>
+          <View style={styles.container}>
+            <View style={styles.compteContainer}>
+              <View style={styles.centerText}>
+                <Text style={styles.boldText}>Les informations du compte</Text>
+              </View>
 
               <View>
                 <Text style={styles.label}>Adresse Email</Text>
@@ -104,197 +199,165 @@ export default function Identity({ navigation }) {
                 />
                 {/* <Text style={styles.forgetPassWordText}>Mot de passe oublié</Text> */}
 
-          <View>
-            <Text style={{ marginLeft: 2, paddingLeft: 10 }}>
-              Numéro de Téléphone
-            </Text>
-            <TextInput
-              style={{
-                height: 40,
-                borderColor: "gray",
-                borderWidth: 1,
-                marginBottom: 10,
-                padding: 8,
-                borderRadius: 10,
-                margin: 15,
-                backgroundColor: "white",
-                marginTop: 5,
-              }}
-              placeholder="Votre numéro de téléphone"
-            />
-          </View>
-
-          <View>
-            <Text style={{ marginLeft: 2, paddingLeft: 10 }}>
-              Date d'inscription
-            </Text>
-
-            <TouchableOpacity
-              onPress={handleOnPressStartDate}
-              style={{
-                height: 40,
-                borderColor: "gray",
-                borderWidth: 1,
-                marginBottom: 10,
-                padding: 8,
-                borderRadius: 10,
-                margin: 15,
-                backgroundColor: "white",
-                marginTop: 5,
-              }}
-            >
-              <Text style={{fontSize: 14}}>{selectedStartDate}</Text>
-            </TouchableOpacity>
-
-            <Modal 
-            animationType="slide"
-            transparent={true}
-            visible={openStartDatePicker}
-            >
-              <View style={styles.centeredView}>
-                <View style={styles.modalView}>
-
-                <DatePicker 
-                  mode="calendar"
-                  minimumDate={startDate}
-                  selected={startedDate}
-                  onDateChanged={handleChangeStartDate}
-                  onSelectedChange={date => setSelectedStartDate(date)}
-                  options={{
-                    backgroundColor: '#00638F',
-                    textHeaderColor: '#fff',
-                    textDefaultColor: '#fff',
-                    selectedTextColor: '#000',
-                    mainColor: '#fff',
-                    textSecondaryColor: '#dbdbdb',
-                    borderColor: '#00638F',
-                  }}
+                <Text style={styles.label}>Numéro de Téléphone</Text>
+                <TextInput
+                  style={
+                    infosUser?.identity?.phoneNumber &&
+                    !inputFocused.phoneNumber
+                      ? styles.savedTextInput
+                      : styles.textContainer
+                  }
+                  value={userData.phoneNumber.toString()}
+                  placeholder={"Votre numéro de téléphone"}
+                  onChangeText={(text) => handleChange("phoneNumber", text)}
+                  onFocus={() => handleFocus("phoneNumber")}
                 />
 
-                  <TouchableOpacity
-                  onPress={handleOnPressStartDate}>
-                    <Text style={{ color: '#fff'}}>Close</Text>
-                  </TouchableOpacity>
+                <Text style={styles.label}>Date d'inscription</Text>
+                {/* <MyDatePicker /> */}
+                <TouchableOpacity
+                  onPress={handleOnPressStartDate}
+                  style={{
+                    height: 40,
+                    borderColor: "gray",
+                    borderWidth: 1,
+                    marginBottom: 10,
+                    padding: 8,
+                    borderRadius: 10,
+                    margin: 15,
+                    backgroundColor: "white",
+                    marginTop: 5,
+                  }}
+                >
+                  <Text style={{fontSize: 14}}>{selectedStartDate}</Text>
+                </TouchableOpacity>
+
+                <Modal 
+                  animationType="slide"
+                  transparent={true}
+                  visible={openStartDatePicker}
+                  >
+                  <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+
+                    <DatePicker 
+                      mode="calendar"
+                      minimumDate={startDate}
+                      selected={startedDate}
+                      onDateChanged={handleChangeStartDate}
+                      onSelectedChange={date => setSelectedStartDate(date)}
+                      options={{
+                        backgroundColor: '#00638F',
+                        textHeaderColor: '#fff',
+                        textDefaultColor: '#fff',
+                        selectedTextColor: '#000',
+                        mainColor: '#fff',
+                        textSecondaryColor: '#dbdbdb',
+                        borderColor: '#00638F',
+                      }}
+                    />
+
+                      <TouchableOpacity
+                      onPress={handleOnPressStartDate}>
+                        <Text style={{ color: '#fff'}}>Close</Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </Modal>
+
+              </View>
+            </View>
+
+            <View style={styles.bar}></View>
+
+            <View style={styles.centerText}>
+              <Text style={styles.boldText}>Informations d'Identité</Text>
+            </View>
+            <View>
+              <Text style={styles.label}>Nom</Text>
+              <TextInput
+                style={
+                  infosUser?.identity?.name && !inputFocused.name
+                    ? styles.savedTextInput
+                    : styles.textContainer
+                }
+                value={userData.name}
+                placeholder="Nom"
+                onChangeText={(text) => handleChange("name", text)}
+                onFocus={() => handleFocus("name")}
+              />
+
+              <Text style={styles.label}>Prénom</Text>
+              <TextInput
+                style={
+                  infosUser?.identity?.firstName && !inputFocused.firstName
+                    ? styles.savedTextInput
+                    : styles.textContainer
+                }
+                value={userData.firstName}
+                placeholder="Prénom"
+                onChangeText={(text) => handleChange("firstName", text)}
+                onFocus={() => handleFocus("firstName")}
+              />
+
+              <Text style={styles.label}>Nationalité</Text>
+              <TextInput
+                style={
+                  infosUser?.identity?.nationality && !inputFocused.nationality
+                    ? styles.savedTextInput
+                    : styles.textContainer
+                }
+                value={userData.nationality}
+                placeholder="Nationalité"
+                onChangeText={(text) => handleChange("Nationalité", text)}
+                onFocus={() => handleFocus("Nationalité")}
+              />
+
+                <Text style={styles.label}>Pays de naissance</Text>
+                <View style={{
+                  height: 40,
+                  borderColor: "gray",
+                  borderWidth: 1,
+                  marginBottom: 10,
+                  borderRadius: 10,
+                  margin: 15,
+                  backgroundColor: "white",
+                  marginTop: 5,
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'flex-start',
+                  alignItems: 'center',
+                }}>
+                <View style={{ 
+                        alignItems: 'flex-start',
+                        width: '80%',
+                        marginLeft: 20,
+                      }}>
+                  <CountryPicker 
+                    {...{
+                      countryCode,
+                      withFilter,
+                      withFlag,
+                      withCountryNameButton,
+                      withAlphaFilter,
+                      withCallingCode,
+                      withEmoji,
+                      onSelect,
+                    }}
+                    
+                  />
+                  
                 </View>
               </View>
 
-            </Modal>
-
-
-            
-          </View>
-
-          <View style={{ 
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center"
-          }}>
-            <View style={styles.bar}></View>
-          </View>
-
-          <View style={{ marginLeft: 2, paddingLeft: 10 }}>
-            <Text>Informations d'Identité</Text>
-          </View>
-          <View>
-            <Text style={{ marginLeft: 2, paddingLeft: 10 }}>Nom</Text>
-            <TextInput
-              style={{
-                height: 40,
-                borderColor: "gray",
-                borderWidth: 1,
-                marginBottom: 10,
-                padding: 8,
-                borderRadius: 10,
-                margin: 15,
-                backgroundColor: "white",
-                marginTop: 5,
-              }}
-              placeholder="Nom"
-            />
-          </View>
-          <View>
-            <Text style={{ marginLeft: 2, paddingLeft: 10 }}>Prénom</Text>
-            <TextInput
-              style={{
-                height: 40,
-                borderColor: "gray",
-                borderWidth: 1,
-                marginBottom: 10,
-                padding: 8,
-                borderRadius: 10,
-                margin: 15,
-                backgroundColor: "white",
-                marginTop: 5,
-              }}
-              placeholder="Prénom"
-            />
-          </View>
-          <View>
-            <Text style={{ marginLeft: 2, paddingLeft: 10 }}>Nationalité</Text>
-            <TextInput
-              style={{
-                height: 40,
-                borderColor: "gray",
-                borderWidth: 1,
-                marginBottom: 10,
-                padding: 8,
-                borderRadius: 10,
-                margin: 15,
-                backgroundColor: "white",
-                marginTop: 5,
-              }}
-              placeholder="Nationalité"
-            />
-          </View>
-
-            <View>
-              <Text style={{ marginLeft: 2, paddingLeft: 10 }}>
-                Pays de naissance
-              </Text>
-            <View style={{
-                height: 40,
-                borderColor: "gray",
-                borderWidth: 1,
-                marginBottom: 10,
-                borderRadius: 10,
-                margin: 15,
-                backgroundColor: "white",
-                marginTop: 5,
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'flex-start',
-                alignItems: 'center',
-              }}>
-              <View style={{ 
-                      alignItems: 'flex-start',
-                      width: '80%',
-                      marginLeft: 20,
-                    }}>
-                <CountryPicker 
-                  {...{
-                    countryCode,
-                    withFilter,
-                    withFlag,
-                    withCountryNameButton,
-                    withAlphaFilter,
-                    withCallingCode,
-                    withEmoji,
-                    onSelect,
-                  }}
-                  
-                />
-                
-              </View>
             </View>
-          </View>
 
-          <View>
+            {/* <View>
             <Text style={{ marginLeft: 2, paddingLeft: 10 }}>
               Situation familiale
             </Text>
-
-          </View>
+            <FamilyPicker style={styles.textContainer} />
+          </View> */}
 
             <View>
               <Text style={styles.label}>Ville de naissance</Text>
@@ -356,8 +419,9 @@ export default function Identity({ navigation }) {
             </View>
           </View>
         </ScrollView>
-    </KeyboardAvoidingView>
-  );
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -445,5 +509,40 @@ const styles = StyleSheet.create({
     borderColor: "grey",
     borderWidth: 0.5,
     margin: 30,
+  },
+  textContainer: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 8,
+    borderRadius: 10,
+    margin: 15,
+    backgroundColor: "#ffffff",
+    marginTop: 5,
+  },
+  savedTextInput: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 8,
+    borderRadius: 10,
+    margin: 15,
+    backgroundColor: "#d3d4d7",
+    marginTop: 5,
+  },
+  actionButton: {
+    backgroundColor: "#277aba",
+    borderRadius: 8,
+    padding: 16,
+    alignItems: "center",
+    marginTop: 46,
+    marginBottom: 16,
+    marginHorizontal: 46,
+  },
+  actionButtonText: {
+    fontSize: 16,
+    color: "#FFFFFF",
   },
 });
